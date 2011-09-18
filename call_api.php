93bf0187d4f50eda17b287014007c9a253859984
rboo <gokcetaskan@gmail.com>
2011-09-18 14:27:45 +0300

    api call moved to php

---
+++ call_api.php
@@ -0,0 +1,28 @@
+<?php
+$allowed_ips = array('127.0.0.1','::1');
+if(!in_array($_SERVER['REMOTE_ADDR'],$allowed_ips)){
+    header("Location: http://www.seismi.org/api");
+    exit;
+} else {
+  $r = (isset($_GET['r'])) ? $_GET['r'] : '';
+  $p = (isset($_GET['p'])) ? $_GET['p'] : '';
+
+  callAPI($r,$p);
+}
+
+function callAPI($resources='',$parameters='') {
+	// Call API
+  $jsonurl = 'http://www.seismi.org/api/'.$resources.'?'.$parameters;
+  $json = file_get_contents($jsonurl,0,null,null);
+  
+  echo $json;
+  
+  // $ch = curl_init();
+  // // set URL and other appropriate options
+  // curl_setopt($ch, CURLOPT_URL, 'http://www.seismi.org/api/eqs/'.resources.'?'.parameters);
+  // curl_setopt($ch, CURLOPT_HEADER, 0);
+  // 
+  // curl_exec($ch);
+  // curl_close($ch);
+}
+?>
