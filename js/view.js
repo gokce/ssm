7490b870af405e7ab7581ceba6cf659f5853cfce
eppan <nikokk@gmail.com>
2011-09-28 21:12:00 +0300

    some style changes

--- js/view.js
+++ js/view.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
 	paper.setup('viewcanvas');
 	var tool = new Tool();
 	
-  var hitOptions = {
-      segments: true,
-      stroke: true,
-      fill: true,
-      tolerance: 5
-  };
-  
-  var current_view = 'show_lst';
-  var canvas;
-  
-  $(window).resize(function() {
-    show(current_view);
-  });
+	var hitOptions = {
+		segments: true,
+		stroke: true,
+		fill: true,
+		tolerance: 5
+	};
+	
+	var current_view = 'show_lst';
+	var canvas;
+	
+	$(window).resize(function() {
+		show(current_view);
+	});
 	
 	$('#navi-nst').click(function(){show('show_nst');});
 	$('#navi-map').click(function(){show('show_map');});
@@ -26,9 +26,9 @@ $(document).ready(function() {
 	$('#navi-lst').click(function(){show('show_lst');});
 	
 	show = function(view_name) {
-	  canvas = {'width':view._viewSize._width, 'height':view._viewSize._height};
-	  current_view = view_name;
-	  window[current_view]($.seismi.data.earthquakes);
+		canvas = {'width':view._viewSize._width, 'height':view._viewSize._height};
+		current_view = view_name;
+		window[current_view]($.seismi.data.earthquakes);
 	}
 	
 	// Placeholders for Infobar
@@ -48,82 +48,85 @@ $(document).ready(function() {
 	holderIndicator['7'] = $('#mag2').find('.i7');
 	
 	$.visualizations = {
-      refresh: function() {
-        $.each($.seismi.data.earthquakes, function(k, v) {
-          initx = Math.random()*view._viewSize._width;
-          inity = Math.random()*-100;
-          initsize = 10;
-          // Circle
-          var eq_circle = new Path.Circle(new Point(initx, inity), initsize);
-          colors = ['#A3CC29','#FFE24D','#CC671F','#B30000']
-    			eq_circle.fillColor = colors[Math.floor(v.magnitude)-4];
-    			eq_circle.opacity = 0.7;
-    			eq_circle.name = 'fill';
-    			
-    			// Selection Stroke
-    			var eq_stroke = new Path.Circle(new Point(initx, inity), initsize);
-    			eq_stroke.strokeWidth = 3;
-    			eq_stroke.originalColor = colors[Math.floor(v.magnitude)-4]; 
-    			eq_stroke.strokeColor = null;
-    			eq_stroke.name = 'stroke';
-    			
-    			var eq_visual = new Group();
-    			eq_visual.addChild(eq_stroke);
-    			eq_visual.addChild(eq_circle);
-    			
-    			// Select
-    			eq_visual['select'] = function() {
-    			  eq_stroke.strokeColor = eq_stroke.originalColor;
-    			}
-    			// Unselect
-    			eq_visual['unselect'] = function() {
-    			  eq_stroke.strokeColor = null;
-    			}
-    			// Return Eq data
-    			eq_visual['data'] = function() {
-    			  return v;
-    			}
-    			
-    			v['eq_visual'] = eq_visual;
-    		  v['move'] = false;
-    		  v['destination'] = -1;
-    		  v['size'] = initsize;
-    		  v['destination_size'] = initsize;
-    		  $.data_loaded = true;
-        });
-        view.draw();
-        show(current_view);
-      }
-  }
-  show_nst = function(data) {
-    
-  }
-  show_map = function(data) {
-    $.each(data, function(k, v) {
-      var point = randomPoint();
-      v['destination'] = point;
-      v['destination_size'] = 10;
-      v['move'] = true;
-    });
-  }
+		refresh: function() {
+			$.each($.seismi.data.earthquakes, function(k, v) {
+				initx = Math.random()*view._viewSize._width;
+				inity = Math.random()*-100;
+				initsize = 6;
+				
+				// Circle
+				var eq_circle = new Path.Circle(new Point(initx, inity), initsize);
+				//colors = ['#A3CC29','#FFE24D','#CC671F','#B30000'] //with eq-colors
+				colors = ['#0044cc','#0044cc','#0044cc','#0044cc'] // only blue
+				opacities = ['0.3','0.5','0.7','0.9']
+				eq_circle.fillColor = colors[Math.floor(v.magnitude)-4];
+				eq_circle.opacity = opacities[Math.floor(v.magnitude)-4];
+				eq_circle.name = 'fill';
+
+				// Selection Stroke
+				var eq_stroke = new Path.Circle(new Point(initx, inity), initsize);
+				eq_stroke.strokeWidth = 3;
+				//eq_stroke.originalColor = colors[Math.floor(v.magnitude)-4]; 
+				eq_stroke.strokeColor = #ffffff;
+				eq_stroke.name = 'stroke';
+
+				var eq_visual = new Group();
+				eq_visual.addChild(eq_stroke);
+				eq_visual.addChild(eq_circle);
+
+				// Select
+				eq_visual['select'] = function() {
+					eq_stroke.strokeColor = eq_stroke.originalColor;
+				}
+				// Unselect
+				eq_visual['unselect'] = function() {
+					eq_stroke.strokeColor = null;
+				}
+				// Return Eq data
+				eq_visual['data'] = function() {
+					return v;
+				}
+
+				v['eq_visual'] = eq_visual;
+				v['move'] = false;
+				v['destination'] = -1;
+				v['size'] = initsize;
+				v['destination_size'] = initsize;
+				$.data_loaded = true;
+			});
+			view.draw();
+			show(current_view);
+		}
+	}
+	show_nst = function(data) {
+
+	}
+	show_map = function(data) {
+		$.each(data, function(k, v) {
+			var point = randomPoint();
+			v['destination'] = point;
+			v['destination_size'] = 10;
+			v['move'] = true;
+		});
+	}
 	
 	show_tml = function(data) {
-	  $.each(data, function(k, v) {
-	    v['eq_visual'].position = randomPoint();
-    });
+		$.each(data, function(k, v) {
+			v['eq_visual'].position = randomPoint();
+		});
 	}
 	
 	show_dpt = function(data) {
-	  posx=100;
-    posy=100;
-	  $.each(data, function(k, v) {
-	    newx = canvas.width-posx;
-	    newy = posy+(Math.floor(v.depth)/1.4);
-	    v['destination'] = new Point(newx,newy);
-	    v['destination_size'] = v.magnitude*5;
-	    v['move'] = true;
-	    posx+=60;
-    });
+		posx=100;
+		posy=100;
+		$.each(data, function(k, v) {
+			newx = canvas.width-posx;
+			newy = posy+(Math.floor(v.depth)/1.4);
+			v['destination'] = new Point(newx,newy);
+			v['destination_size'] = v.magnitude*5;
+			v['move'] = true;
+			posx+=60;
+		});
 	}
 	
 	show_lst = function(data) {
@@ -149,8 +152,7 @@ $(document).ready(function() {
 					text.characterStyle = {
 						fontSize: 14,
 						fillColor: 'white',
-						font: 'extravaganzzaBold', //somehow not working
-						//font: 'sans-serif',
+						font: 'extravaganzzaBold',
 					};
 					text.content=v.day;
 					*/
@@ -163,7 +165,7 @@ $(document).ready(function() {
 				posy+=30;
 			}
 			v['destination'] = new Point(posx, posy);
-			v['destination_size'] = 10;
+			v['destination_size'] = ((v.magnitude * 2.5) - 5);
 			v['move'] = true;
 			posx+=25;
 			prev_day=v.day;
@@ -171,65 +173,65 @@ $(document).ready(function() {
 	}
 		
 	randomPoint = function() {
-	  var point = new Point(0, 0);
-    //var point = Point.random() * view.size; //NaN
-    point.x = Math.random() * view.size.width;
-    point.y = Math.random() * view.size.height;
-	  return point
+		var point = new Point(0, 0);
+		//var point = Point.random() * view.size; //NaN
+		point.x = Math.random() * view.size.width;
+		point.y = Math.random() * view.size.height;
+		return point
 	}
 	vectorDiff = function(dest, src, speed) {
-	  var new_pos = new Point(0,0);
-	  var diff_x = dest.x - src.x;
-	  var diff_y = dest.y - src.y;
-	  new_pos.x = src.x + (diff_x/speed);
-	  new_pos.y = src.y + (diff_y/speed);
-	  dist = Math.sqrt(Math.pow(diff_x,2) + Math.pow(diff_y,2));
-	  return {dist : dist, new_pos : new_pos};
+		var new_pos = new Point(0,0);
+		var diff_x = dest.x - src.x;
+		var diff_y = dest.y - src.y;
+		new_pos.x = src.x + (diff_x/speed);
+		new_pos.y = src.y + (diff_y/speed);
+		dist = Math.sqrt(Math.pow(diff_x,2) + Math.pow(diff_y,2));
+		return {dist : dist, new_pos : new_pos};
 	}
 	view.onFrame = function(event) {
-	  if ($.data_loaded) {
-      $.each($.seismi.data.earthquakes, function(k, v) {
-        if (v['move']) {
-          var obj = v['eq_visual'];
-          var vector = v['destination'] - obj.position;
-          var vd = vectorDiff(v['destination'], obj.position, 4);
-          obj.position = vd.new_pos;
-          if (vd.dist < 1) {
-            v['move'] = false;
-          }
-          if (v['destination_size'] != v['size']) {
-            obj.scale(v['destination_size']/v['size']);
-            v['size'] = v['destination_size'];
-          }
-        }
-      });
-    }
-  }
-  tool.onMouseDown = function(event) {
-    var hitResult = project.hitTest(event.point, hitOptions);
-    // Clear selected
-    $.each(project.activeLayer.children, function(k, v) {
-      v.unselect();
-    });
-    // Show selected
-    if (hitResult && hitResult.item) {
-        hitResult.item.parent.select();
-        // Put selected data into Infobar
-        refreshInfobar(hitResult.item.parent.data());
-    }
-  }
-  refreshInfobar = function(data) {
-    //data.day, data.depth, data.eqid, data.lat, data.lon, data.magnitude, data.region, data.src, data.time, data.timedate
-    $(holderMagnitude).html(data.magnitude);
+		if ($.data_loaded) {
+			$.each($.seismi.data.earthquakes, function(k, v) {
+				if (v['move']) {
+					var obj = v['eq_visual'];
+					var vector = v['destination'] - obj.position;
+					var vd = vectorDiff(v['destination'], obj.position, 4);
+					obj.position = vd.new_pos;
+					if (vd.dist < 1) {
+						v['move'] = false;
+					}
+					if (v['destination_size'] != v['size']) {
+						obj.scale(v['destination_size']/v['size']);
+						v['size'] = v['destination_size'];
+					}
+				}
+			});
+		}
+	}
+	tool.onMouseDown = function(event) {
+		var hitResult = project.hitTest(event.point, hitOptions);
+		// Clear selected
+		$.each(project.activeLayer.children, function(k, v) {
+			v.unselect();
+		});
+		// Show selected
+		if (hitResult && hitResult.item) {
+			hitResult.item.parent.select();
+			// Put selected data into Infobar
+			refreshInfobar(hitResult.item.parent.data());
+		}
+	}
+	refreshInfobar = function(data) {
+		//data.day, data.depth, data.eqid, data.lat, data.lon, data.magnitude, data.region, data.src, data.time, data.timedate
+		$(holderMagnitude).html(data.magnitude);
 		$(holderDepth).html(Math.round(data.depth));
 		$(holderTime).html(data.time);
 		$(holderDate).html(data.day);
 		$(holderRegion).html(truncate(data.region,35));
 		$(holderLatlon).html(data.lat+'&deg;&nbsp;&nbsp;'+data.lon+'&deg;');
-    $.each(holderIndicator, function(k, v) { v.removeClass('isel'); });
+		$.each(holderIndicator, function(k, v) { v.removeClass('isel'); });
 		$(holderIndicator[Math.floor(data.magnitude)]).addClass('isel');
-  }
-  // String Truncate
+	}
+	// String Truncate
 	var truncate = function (str, limit) {
 		var bits, i;
 		bits = str.split('');
