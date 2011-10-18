7eab0af1f8d50e648ec2c14df700d4046462925b
eppan <nikokk@gmail.com>
2011-10-18 12:13:50 +0300

    fixed beginning states for eq-level

--- seismi.php
+++ seismi.php
@@ -41,7 +41,7 @@
 			<li class="lst" title="List View">LST</li>
 			<li class="hlp" title="Help">?</li>
 		</ul>
-		<div id="mag1"><div id="mag2"><a href="#" class="tt" title="Depth for selected earthquake"><em id="depth">0</em><em id="depthunit">km</em></a><a href="#" class="tt" title="Magnitude value for selected earthquake"><em id="magnitude">0.0</em></a><div class="i4"></div><div class="i5"></div><div class="i6 isel"></div><div class="i7"></div></div></div>
+		<div id="mag1"><div id="mag2"><a href="#" class="tt" title="Depth for selected earthquake"><em id="depth">0</em><em id="depthunit">km</em></a><a href="#" class="tt" title="Magnitude value for selected earthquake"><em id="magnitude">0.0</em></a><div class="i4"></div><div class="i5"></div><div class="i6"></div><div class="i7"></div></div></div>
 		<div id="tec1"><div id="tec2"><a href="#" class="tt disable" title="show/hide tectonic plates"><em id="tectonic"><span>0FF</span><span>0N</span></em></a></div></div>
 		<div id="vol1"><div id="vol2"><a href="#" class="tt disable" title="show/hide volcanoes"><em id="volcanoes"><span>0FF</span><span>0N</span></em></a></div></div>
 		<div id="tim1"><div id="tim2"><a href="#" class="tt" title="Date and UTC (Coordinated Universal Time) time for selected earthquake"><em id="date">01.01.2010</em><em id="time"><span id="utc">UTC</span>00:00</em></a></div></div>
