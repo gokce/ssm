36a6da595541b9b11c1afd3ba7d6397633930ba0
eppan <nikokk@gmail.com>
2011-10-20 00:59:47 +0300

    new beginning pics

--- seismi.php
+++ seismi.php
@@ -122,7 +122,7 @@
 		<div id="help1"></div><!--<div id="help2"></div>--><div id="help3"></div><div id="help4"></div>
 	</div>
 	<div id="intro"></div>
-	<div id="intro-container"><img src="images/beginning.png" id="beginning-img" width="500" height="400" alt="beginning info" /><a href="seismi.php" id="tryagain"><img src="images/button-tryagain.png" width="300" height="100" alt="problems loading, please try again" /></a></div>
+	<div id="intro-container"><img src="images/beginning.png" id="beginning-middle" width="260" height="290" alt="beginning info" /><img src="images/beginning-corner.png" id="beginning-corner" width="200" height="135" alt="beginning info" /><a href="seismi.php" id="tryagain"><img src="images/button-tryagain.png" width="300" height="100" alt="problems loading, please try again" /></a></div>
 	<?php 
 	$ip = $_SERVER["SERVER_ADDR"];
 	if ($ip == '::1' || $ip == '127.0.0.1') {
